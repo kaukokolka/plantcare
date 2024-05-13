b35f494fedad6079e0e2e61155f68e3c72c097c9
kaukokolka <nils@fedoto.ws>
2024-05-13 20:32:00 +0300

    user registration

--- app.js
+++ app.js
@@ -60,12 +60,12 @@ app.post('/validate', (req, res) => {
     db.get('SELECT * FROM Users WHERE username = ? AND password = ?', [username, password], (err, row) => {
         if (err)  {
             console.error('internal server error after login:', err);
-            res.status(500).json({ error: 'Internal Server Error' }); //500 internal server error
+            res.status(500).send('Internal Server Error'); //500 internal server error
             return;
         }
         if (!row) {
             console.log('Incorrect username or password');
-            res.status(401).json({ error: 'Incorrect username or password' }); //401 unauthorized
+            res.status(401).send('Incorrect username or password'); //401 unauthorized
             return;
         }
         const userId = row.user_id;
@@ -76,31 +76,54 @@ app.post('/validate', (req, res) => {
 });
 
 app.post('/newuser', (req, res) => { //CHECK IF USERNAME EXISTS, +INCLUDE REPEAT-PASS, +CHECK SYMBOLS
-    const { username } = req.body;
-    const password = sha256(req.body.password);
-    console.log(username, password);
-    db.run('INSERT INTO Users(username, password) VALUES(?, ?)', [username, password], function(err) {
-        if (err)  {
-            console.error('internal server error after creating user:', err);
-            res.status(500).json({ error: 'Internal Server Error' }); //500 internal server error
-            return;
-        }
-        console.log('User is created!'); // Log successful account creation attempt
-        res.redirect('/login');
+    const { username, password, repeatPassword } = req.body;
+    db.get('SELECT 1 FROM Users WHERE username = ?', [username], (err, row) => { //check if username already exists
+      if (err) {
+        console.error('internal server error querying username:', err);
+        res.status(500).send('Internal Server Error'); //500 internal server error
+        return;
+      }
+      if (row) { //check whether a row with such username already exists
+        res.status(400).send('Username already exists')
+        return;
+      }
+      if (isAlphaNumeric(username) == false) { //check whether username only includes allowed symbols
+        res.status(400).send('Username includes unpermitted symbols. Please only use alphanumeric symbols(a-z, A-Z, 0-9)')
+        return;
+      } //Username OK, move on
+      if (password !== repeatPassword) { //check whether password fields match
+        res.status(400).send('Passwords do not match');
+        return;
+      }
+      if (isAcceptable(password) == false) { //check whether password only includes allowed symbols
+        res.status(400).send('Username includes unpermitted symbols. Please only use alphanumeric symbols(a-z, A-Z, 0-9) and common special symbols(#?!@$%^&-*)');
+        return;
+      } //Password OK, move on
+      const hashedPassword = sha256(password); //hash before inserting
+      console.log(username, hashedPassword);
+      db.run('INSERT INTO Users(username, password) VALUES(?, ?)', [username, hashedPassword], function(err) { //create new row(user) in db
+          if (err)  {
+              console.error('internal server error after creating user:', err);
+              res.status(500).send('Internal Server Error'); //500 internal server error
+              return;
+          }
+          console.log('User is created!'); // Log successful account creation attempt
+          res.redirect('/login');
+      });
     });
 });
 
-app.get('/user', requireAuth, function (req, res) {
+app.get('/user', requireAuth, function (req, res) { //for fetching user data. to be used during active session
     const userId = req.session.userId;
     db.get('SELECT * FROM Users WHERE user_id = ?', [userId], (err, row) => {
         if (err) {
             console.error('internal server error querying ID:', err);
-            res.status(500).json({ error: 'Internal Server Error' }); //500 internal server error
+            res.status(500).send('Internal Server Error'); //500 internal server error
             return;
         }
         if (!row) {
             console.log('User not Found');
-            res.status(404).json({ error: '404 Not Found' }); //404 not found
+            res.status(404).send('404 Not Found'); //404 not found
             return;
         }
         console.log('user: success!');
@@ -109,17 +132,17 @@ app.get('/user', requireAuth, function (req, res) {
     });
 });
 
-app.get('/plants/:id', requireAuth, (req,res) => {
+app.get('/plants/:id', requireAuth, (req,res) => { //dynamically render EJS template about plant when requested
     const plantId = req.params.id;
     db.get('SELECT * FROM Plants WHERE plant_id = ?', [plantId], (err, row) => {
         if (err) {
             console.error('internal server error querying ID:', err);
-            res.status(500).json({ error: 'Internal Server Error' }); //500 internal server error
+            res.status(500).send('Internal Server Error'); //500 internal server error
             return;
         }
         if (!row) {
             console.log('Plant not Found');
-            res.status(404).json({ error: '404 Not Found' }); //404 not found
+            res.status(404).send('404 Not Found'); //404 not found
             return;
         }
         console.log('id: success!')
@@ -127,7 +150,7 @@ app.get('/plants/:id', requireAuth, (req,res) => {
     });
 });
 
-app.get('/data', requireAuth, (req, res) => {
+app.get('/data', requireAuth, (req, res) => { //for fetching the entire plant data.
     db.all('SELECT * FROM Plants', (err, rows) => {
         if (err) {
             console.error(err.message);
@@ -140,18 +163,15 @@ app.get('/data', requireAuth, (req, res) => {
     });
 });
 
-//app.get('/username', (req, res) => {
-//    db.all('SELECT * FROM Users', (err, rows) => { //FINISH HERE WHERE username = ?
-//        if (err) {
-//            console.error(err.message);
-//            res.status(500).send('Internal Server Error');
-//        } else {
-//            // Send JSON response with fetched data
-//            res.setHeader('Content-Type', 'application/json');
-//            res.json(rows);
-//        }
-//    });
-//});
+function isAlphaNumeric(input) {
+  const regex = /^[a-zA-Z0-9]+$/;
+  return regex.test(input);
+}
+
+function isAcceptable(input) {
+  const regex = /^[a-z0-9#?!@$%^&\-*]+$/i; //a-z0-9#?!@$%^&-*
+  return regex.test(input);
+}
 
 function sha256(hashable) {
   return crypto.createHash('sha256').update(hashable).digest('hex');
