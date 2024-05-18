65ce14cc84f8fb2f7e851767ab62e2bb0f4dabbc
kaukokolka <nils@fedoto.ws>
2024-05-18 20:02:57 +0300

    upd8

--- app.js
+++ app.js
@@ -112,11 +112,11 @@ app.post('/newuser', (req, res) => { //CHECK IF USERNAME EXISTS, +INCLUDE REPEAT
         return;
       }
       if (row) { //check whether a row with such username already exists
-        res.status(400).send('Username already exists')
+        res.status(400).send('Username already exists');
         return;
       }
       if (isAlphaNumeric(username) == false) { //check whether username only includes allowed symbols
-        res.status(400).send('Username includes unpermitted symbols. Please only use alphanumeric symbols(a-z, A-Z, 0-9)')
+        res.status(400).send('Username includes unpermitted symbols. Please only use alphanumeric symbols(a-z, A-Z, 0-9)');
         return;
       } //Username OK, move on
       if (password !== repeatPassword) { //check whether password fields match
@@ -124,7 +124,7 @@ app.post('/newuser', (req, res) => { //CHECK IF USERNAME EXISTS, +INCLUDE REPEAT
         return;
       }
       if (isAcceptable(password) == false) { //check whether password only includes allowed symbols
-        res.status(400).send('Username includes unpermitted symbols. Please only use alphanumeric symbols(a-z, A-Z, 0-9) and common special symbols(#?!@$%^&-*)');
+        res.status(400).send('Password includes unpermitted symbols. Please only use alphanumeric symbols(a-z, A-Z, 0-9) and common special symbols(#?!@$%^&-*)');
         return;
       } //Password OK, move on
       const hashedPassword = sha256(password); //hash before inserting
@@ -159,9 +159,13 @@ app.post('/newlog', requireAuth, (req, res) => {
       });
     });
 
-app.post('/newplant', requireAuth, (req, res) => {
+app.post('/newplant', requireAuth, requireAdmin, (req, res) => {
     const { number, name, summerLocation, schooltimeLocation, frequency } = req.body;
-    console.log(name, summerLocation, schooltimeLocation, frequency);
+    if (isBlank(number) || isBlank(name) || isBlank(summerLocation) || isBlank(schooltimeLocation) || isBlank(frequency)) {
+      res.status(400).send('Lūdzu aizpildiet visus laukumus, lai pievienotu augu.')
+      return;
+    }
+    console.log("all isBlanks False: ", name, summerLocation, schooltimeLocation, frequency);
     db.get('SELECT 1 FROM Plants WHERE number = ?', [number], (err, row) => { //check if username already exists
       if (err) {
         console.error('internal server error querying number:', err);
@@ -183,6 +187,47 @@ app.post('/newplant', requireAuth, (req, res) => {
       });
     });
   });
+
+  app.post('/editplant', requireAuth, requireAdmin, (req, res) => {
+      const { plantId, oldNumber, number, name, summerLocation, schooltimeLocation, frequency } = req.body;
+      if (isBlank(number) || isBlank(name) || isBlank(summerLocation) || isBlank(schooltimeLocation) || isBlank(frequency)) {
+        res.status(400).send('Lūdzu aizpildiet visus laukumus, lai veiksmīgi rediģētu augu.')
+        return;
+      }
+      console.log("all isBlanks False: ", name, summerLocation, schooltimeLocation, frequency);
+      if (oldNumber != number) { //if number has been edited
+        db.get('SELECT 1 FROM Plants WHERE number = ?', [number], (err, row) => { //check if the new number is already taken by another plant
+          if (err) {
+            console.error('internal server error querying number:', err);
+            res.status(500).send('Internal Server Error'); //500 internal server error
+            return;
+          }
+          if (row) { //check whether a row with such number already exists
+            res.status(400).send('Augs ar šādu numuru jau pastāv, lūdzu izvēlieties citu.')
+            return;
+          }
+          db.run('UPDATE Plants SET number = ?,name = ?, summer_location = ?, schooltime_location = ?, frequency = ? WHERE plant_id = ?', [number, name, summerLocation, schooltimeLocation, frequency, plantId], function(err) { //create new row(log) in db
+              if (err)  {
+                  console.error('internal server error after creating plant:', err);
+                  res.status(500).send('Internal Server Error'); //500 internal server error
+                  return;
+              }
+              console.log('Plant has been edited!'); // Log successful log creation attempt
+              res.redirect('/plants/' + plantId); //reload back into page, now with the edited plant
+        });
+      });
+        } else {
+        db.run('UPDATE Plants SET number = ?,name = ?, summer_location = ?, schooltime_location = ?, frequency = ? WHERE plant_id = ?', [number, name, summerLocation, schooltimeLocation, frequency, plantId], function(err) { //create new row(log) in db
+            if (err)  {
+                console.error('internal server error after creating plant:', err);
+                res.status(500).send('Internal Server Error'); //500 internal server error
+                return;
+            }
+            console.log('Plant has been edited!'); // Log successful log creation attempt
+            res.redirect('/plants/' + plantId); //reload back into page, now with the edited plant
+        });
+      }
+    });
 
 app.get('/user', requireAuth, function (req, res) { //for fetching user data. to be used during active session
     const userId = req.session.userId;
@@ -229,6 +274,24 @@ app.get('/plants/:id', requireAuth, (req,res) => { //dynamically render EJS temp
     });
 });
 
+app.get('/plants/:id/edit', requireAuth, requireAdmin, (req,res) => { //dynamically render EJS template about plant when requested
+    const plantId = req.params.id;
+    db.get('SELECT * FROM Plants WHERE plant_id = ?', [plantId], (err, row) => {
+        if (err) {
+            console.error('internal server error querying ID:', err);
+            res.status(500).send('Internal Server Error'); //500 internal server error
+            return;
+        }
+        if (!row) {
+            console.log('Plant not Found');
+            res.status(404).send('404 Not Found'); //404 not found
+            return;
+        }
+        console.log('id: success!')
+        res.render('plantedit.ejs', { plant: row });
+    });
+});
+
 app.get('/data', requireAuth, (req, res) => { //for fetching the entire plant data.
     db.all('SELECT * FROM Plants', (err, rows) => {
         if (err) {
@@ -250,6 +313,10 @@ function isAlphaNumeric(input) {
 function isAcceptable(input) {
   const regex = /^[a-z0-9#?!@$%^&\-*]+$/i; //a-z0-9#?!@$%^&-*
   return regex.test(input);
+}
+
+function isBlank(str) {
+   return str.trim().length === 0;
 }
 
 function sha256(hashable) {
